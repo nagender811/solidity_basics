83398232aaabae4dd93eb7e086eeabd7a5187e6e
Nagender kumar <kumarnagender811@gmail.com>
2025-12-28 19:34:41 +0530

    Learned if-else statements in Solidity

---
+++ 08_if_else.sol
@@ -0,0 +1,19 @@
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract IfElse 
+{
+    function check(int a) public pure returns(string memory)
+    {
+        string memory value;
+        if (a>0){
+            value="Greater than zero";
+        }
+        else if(a==0){
+            value="Equal to zero";
+        }
+        else {
+            value="Less than zero";
+        }
+        return value;
+    }
+}
