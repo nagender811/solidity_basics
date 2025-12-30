087299bdef8fc4305eb0f7b60412adc5e1f301b7
Nagender kumar <kumarnagender811@gmail.com>
2025-12-30 11:39:16 +0530

    removed accidently added file

---
+++ 15_lottery_project.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract Lottery {
+    address public manager;
+    address payable[] public participants;
+
+    constructor()
+    {
+        manager = msg.sender; //global variable
+    }
+
+    receive() external payable {
+        require(msg.value == 1 ether);
+        participants.push(payable(msg.sender));
+     }
+
+     function getBalance() public view returns(uint)
+     {
+        require(msg.sender == manager);
+        return address(this).balance;
+     }
+}
