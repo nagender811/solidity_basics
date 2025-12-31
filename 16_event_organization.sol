7e5c56b772497fae740defe60594ebfe54316182
Nagender kumar <kumarnagender811@gmail.com>
2025-12-31 12:14:03 +0530

    Add createEvent function

--- 16_event_organization.sol
+++ 16_event_organization.sol
@@ -2,5 +2,24 @@
 pragma solidity >= 0.5.0 < 0.9.0;
 
 contract EventContract {
-    
+    struct Event {
+        address organizer;
+        string name;
+        uint date;
+        uint price; 
+        uint ticketCount;
+        uint ticketRemain;
+    }
+
+    mapping (uint => Event) public events;
+    mapping (address => mapping (uint=>uint)) public tickets;
+    uint public nextId;
+
+    function createEvent(string memory name, uint date, uint price, uint ticketCount) external {
+        require(date>block.timestamp, "You can oraganize event for future date");
+        require(ticketCount>0, "You can organize event only if you create more than 0 tickets");
+
+        events[nextId] = Event(msg.sender, name, date, price, ticketCount, ticketCount);
+        nextId++;
+    }
 }
