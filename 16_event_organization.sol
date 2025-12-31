127b38159e1f4eb0faf738c35343ad1c03296b28
Nagender kumar <kumarnagender811@gmail.com>
2025-12-31 12:30:43 +0530

    Add payable buyTicket function

--- 16_event_organization.sol
+++ 16_event_organization.sol
@@ -22,4 +22,14 @@ contract EventContract {
         events[nextId] = Event(msg.sender, name, date, price, ticketCount, ticketCount);
         nextId++;
     }
+
+    function buyTicket(uint id, uint quantity) external payable {
+        require(events[id].date != 0, "Event does not exist");
+        require(events[id].date > block.timestamp, "Event has already occured");
+        Event storage _event = events[id];
+        require(msg.value == (_event.price*quantity), "Ether is not enough");
+        require(_event.ticketRemain >= quantity, "Not enough tickets");
+        _event.ticketRemain -= quantity;
+        tickets[msg.sender][id]+=quantity;
+    }
 }
