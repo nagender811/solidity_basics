fe639bffd9b9c46865b8a9818afda9b71217feb7
Nagender kumar <kumarnagender811@gmail.com>
2026-01-02 14:44:31 +0530

    Added request management feature to smart contract

--- 17_crowd_funding.sol
+++ 17_crowd_funding.sol
@@ -10,6 +10,18 @@ contract CrowdFunding{
     uint public raisedAmount;
     uint public noOfContributors;
 
+    struct Request{
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address=>bool) voters;
+    }
+    
+    mapping(uint => Request) public requests;
+    uint public numRequests;
+
     constructor(uint _target, uint _deadline){
         target = _target;
         deadline = block.timestamp+_deadline;  // 10sec + 3600sec (60*60)
@@ -38,5 +50,20 @@ contract CrowdFunding{
         address payable user = payable(msg.sender);
         user.transfer(contributors[msg.sender]);
         contributors[msg.sender] = 0;
+    }
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only manager can call this function");
+        _;
+    }
+
+    function createRequests(string memory _description, address payable _recipient, uint _value) public  onlyManager{
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;
     }
 }
