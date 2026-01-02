9b7c39c34e18184f50f9c994dc4cfadc11a1d07a
Nagender kumar <kumarnagender811@gmail.com>
2026-01-02 13:34:43 +0530

    Enhanced event contract with balance tracking and refund mechanism

--- 17_crowd_funding.sol
+++ 17_crowd_funding.sol
@@ -2,7 +2,7 @@
 pragma solidity >= 0.5.0 < 0.9.0;
 
 contract CrowdFunding{
-    mapping (address => uint) public contributors;
+    mapping (address => uint) public contributors;   //contributors[msg.sender] = 100
     address public manager;
     uint public minimumContribution;
     uint public deadline;
@@ -11,7 +11,7 @@ contract CrowdFunding{
     uint public noOfContributors;
 
     constructor(uint _target, uint _deadline){
-        target = target;
+        target = _target;
         deadline = block.timestamp+_deadline;  // 10sec + 3600sec (60*60)
         minimumContribution = 100 wei;
         manager = msg.sender;
@@ -26,5 +26,17 @@ contract CrowdFunding{
         }
         contributors[msg.sender]+= msg.value;
         raisedAmount+=msg.value;
+    }
+
+    function getContractBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function refund() public {
+        require(block.timestamp > deadline && raisedAmount < target, "You are not eligible for refund");
+        require(contributors[msg.sender]>0);
+        address payable user = payable(msg.sender);
+        user.transfer(contributors[msg.sender]);
+        contributors[msg.sender] = 0;
     }
 }
